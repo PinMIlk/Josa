45246cb8adbd814b6725aaab7abb1d63c7263cc2
Hanbyeol Jo <60456752+PinMilk@users.noreply.github.com>
2020-09-09 22:24:22 +0900

    Create josa.ts

---
+++ josa.ts
@@ -0,0 +1,63 @@
+/**
+ * @param {string} word - Word to set Josa
+ * @param {string} class - Class of Josa
+ * 
+ * @returns {string} Word and correct Josa
+ * 
+ * @example
+ * console.log(new Josa('사람', '와').execute());
+ */
+
+class Josa {
+    public word: string;
+    public class: string;
+
+    constructor(word: string, wordClass: string) {
+        const format: any = {
+            은: '은',
+            이: '이',
+            을: '을',
+            과: '과',
+            으로: '으로',
+            는: '은',
+            가: '이',
+            를: '을',
+            와: '과',
+            로: '으로'
+        };
+
+        this.word = word;
+        this.class = format[wordClass];
+    }
+
+    private isHangul() {
+        const charCode: number = this.word.charCodeAt(this.word.length - 1) - 44032;
+        if (charCode < 0 || charCode > 11171) return false;
+        return true;
+    }
+
+    private hasLast() {
+        return ((this.word.charCodeAt(this.word.length - 1) - 44032) % 28 > 0) ? true : false;
+    }
+
+    public execute() {
+        const hasLast = this.hasLast();
+
+        if (!this.isHangul()) throw new ReferenceError('Word is not Hangul.');
+
+        switch (this.class) {
+            case '은':
+                return this.word + (hasLast ? '은' : '는');
+            case '이':
+                return this.word + (hasLast ? '이' : '가');
+            case '을':
+                return this.word + (hasLast ? '을' : '를');
+            case '과':
+                return this.word + (hasLast ? '과' : '와');
+            case '으로':
+                return this.word + (hasLast ? '으로' : '로')
+            default:
+                return this.word + this.class;
+        }
+    }
+}
